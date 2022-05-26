a613e98eeec798696a9e2ed6930e5883a2b42326
Serhii Hromov <melifarowow@gmail.com>
2022-05-26 23:53:33 +0300

    cleaner?

--- react-menu.js
+++ react-menu.js
@@ -1,5 +1,34 @@
 'use strict';
 
+/* TODO: is it ok to move this part from index.html just because I have to change parent's class? */
+function MenuLinks() {
+    return (
+        <ul className="menu-links">
+            <li>
+                <a href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="settings.html">Settings</a>
+            </li>
+        </ul>
+    );
+}
+
+const spanBars = [...Array(3)].map((_,index) => <span key={index} className="bar"></span>)
+
+//TODO: is it common practice (onPress) or I'd better move it directly inside Toggle Menu's render()?
+const ToggleButton = ({ onPress }) => {
+    return (
+        <div className="toggle" id="toggle-button" onClick={onPress}>
+            {spanBars}
+            {/* TODO: or the old one is better */}
+            {/* <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span> */}
+        </div>
+    )
+}
+
 class ToggleMenu extends React.Component {
     constructor(props) {
         super(props);
@@ -7,7 +36,6 @@ class ToggleMenu extends React.Component {
     }
 
     handleClick = () => {
-        console.log(this)
         this.setState({
             active: !this.state.active
         });
@@ -16,21 +44,8 @@ class ToggleMenu extends React.Component {
     render() {
         return (
             <div className={`react-menu ${this.state.active ? 'active' : ''}`}>
-                <div className="toggle" id="toggle-button" onClick={this.handleClick}>
-                    {/* TODO: is it ok or has to be done with help of map? */}
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </div>
-                {/* TODO: is it ok to move this part from index.html? */}
-                <ul class="menu-links">
-                    <li>
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                        <a href="settings.html">Settings</a>
-                    </li>
-                </ul>
+                <ToggleButton onPress={this.handleClick} />
+                <MenuLinks />
             </div>
         )
     }
